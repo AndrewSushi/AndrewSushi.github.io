b5186ea5376a5354c797216dac7540e03936f502
AndrewSushi <AndrewSushi8@gmail.com>
2023-03-02 00:17:09 -0800

    Added files and started with the basic skeleton and href's

---
+++ app.js
@@ -0,0 +1,11 @@
+// This is optional, but you can add some interactivity with JavaScript.
+// For example, smooth scrolling to anchor links:
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
