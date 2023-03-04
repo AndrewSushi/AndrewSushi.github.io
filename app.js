d0bbc1245bc4ed70162757ebb8317a4af1d6cc0d
AndrewSushi <AndrewSushi8@gmail.com>
2023-03-03 23:42:01 -0800

    Finished Website

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-// This is optional, but you can add some interactivity with JavaScript.
-// For example, smooth scrolling to anchor links:
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        var scrollTop = section.offsetTop - 125; // adjust the offset value as needed
+        window.scrollTo({
+            top: scrollTop,
             behavior: 'smooth'
         });
+    }
+}
+  
+var navLinks = document.querySelectorAll('nav a');
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        var sectionId = this.getAttribute('href').replace('#', '');
+        scrollToSection(sectionId);
     });
+}
+  
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
